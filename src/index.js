6163cb643f4df85d1e3cce5bac86140ecbeedc20
Andrea Bender <andreab.1726@gmail.com>
2019-10-07 12:30:08 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import STORE from './store.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
